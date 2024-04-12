8987bd52ba0306a9c8264f0df8752b2590c8fc7d
Utkarsh <lovekickstartsyadav@gmail.com>
2024-04-12 09:57:22 +0530

    car id

---
+++ problem1.js
@@ -0,0 +1,15 @@
+function problem1(arr, value) {
+    // Check if arr is an array
+    if (!Array.isArray(arr)) {
+      return ('Inventory should be of type array');
+    }
+    // Check if  thevalue is greater than 0
+    if (value <= 0) {
+      return ('Id should be greater than 0');
+    }
+    // Find the car object with the given id
+    return arr.find(car => car.id === value);
+}
+
+module.exports = problem1;
+  
